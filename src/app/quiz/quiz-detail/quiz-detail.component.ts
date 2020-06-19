923f7287dfc8f0da105734ec296f5fba14f626c3
CDDelta <ngjunyen@gmail.com>
2020-06-20 01:33:56 +1000

    add quiz payment pointer when user is playing

--- src/app/quiz/quiz-detail/quiz-detail.component.ts
+++ src/app/quiz/quiz-detail/quiz-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 import { Quiz } from 'src/app/models/quiz';
+import { MonetizationService } from 'ngx-monetization';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz-detail',
@@ -10,11 +12,23 @@ import { Quiz } from 'src/app/models/quiz';
 export class QuizDetailComponent implements OnInit {
   public quiz: Quiz;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private monetization: MonetizationService,
+  ) {}
 
   ngOnInit(): void {
     this.route.data.subscribe((data: { quiz: Quiz }) => {
       this.quiz = data.quiz;
+
+      if (this.quiz.paymentPointer)
+        this.monetization.setPaymentPointer(this.quiz.paymentPointer);
     });
+
+    // Remove payment pointer when user navigates away from quiz.
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationStart))
+      .subscribe(() => this.monetization.setPaymentPointer(''));
   }
 }
